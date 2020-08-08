9e4ed55012e614ac0505a5a5cfea0dfa988ff178
Andreas Reinle <gras64@web.de>
2020-08-08 16:13:02 +0200

    first update

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/car-side.svg" card_color="#BDC3C7" width="50" height="50" style="vertical-align:bottom"/> Car Control Example
-You can control a car wit light clima and other things
+with this mycroft skill you can control things in the car such as air conditioning, lights, wipers, cruise control and LED lighting
 
 ## About
-Same
+The skill was used as an interface for controlling the electronics in a car. It only contains the basic code without an API. obd2 or airplay would be conceivable, depending on the vehicle, the api commands must be set up.
 
 ## Examples
 * "Switch light on"
+* "how much battery capacity I have left"
+* "make everything dark"
+* "Interior lighting in red"
+* "Deactivate low beam"
+* "Cruise control to 100 km/h"
+* "start charging"
+* "Stop loading"
+* "Set temperature to 20"
+* "make you colder"
+* "Temperature increase"
+* "delete cruise control"
+* "speed up wipers"
+* "slow down wipers"
+* "disable wipers"
+* "aktivate wipers"
 
 ## Credits
 gras64
--- __init__.py
+++ __init__.py
@@ -22,6 +22,12 @@ class CarControlExample(MycroftSkill):
         self.register_intent_file('windshield.wipers.off.intent', self.handle_windshield_off_wipers)
         self.register_intent_file('windshield.wipers.faster.intent', self.handle_windshield_faster_wipers)
         self.register_intent_file('windshield.wipers.lower.intent', self.handle_windshield_lower_wipers)
+        ### handling messages
+        self.add_event('enclosure.mouth.text',
+                self.send_enclosure_text)
+
+    def send_enclosure_text(self):
+        pass
 
     def handle_light_on(self, message):
         self.speak_dialog('light.on')
@@ -30,7 +36,7 @@ class CarControlExample(MycroftSkill):
         self.speak_dialog('light.off')
 
     def handle_start_loading(self, message):
-        percent = "30"
+        percent = message.data.get("speed", "100")
         self.speak_dialog('start.loading', data={'percent':percent})
 
     def handle_stop_loading(self, message):
@@ -43,29 +49,29 @@ class CarControlExample(MycroftSkill):
         self.speak_dialog('bat.state', data={'percent':percent, 'range':range})
 
     def handle_temperature_up(self, message):
-        temperature = "20"
+        temperatur = message.data.get("position", "20")
         self.speak_dialog('temperature', data={'temperature':temperature})
 
     def handle_temperature_down(self, message):
-        temperature = "20"
+        temperatur = message.data.get("position", "20")
         self.speak_dialog('temperature', data={'temperature':temperature})
 
     def handle_set_cruise_control(self, message):
-        speed = "120"
+        speed = message.data.get("speed", "120")
         self.speak_dialog('set.cruise.control', data={'speed':speed})
 
     def handle_unset_cruise_control(self, message):
         self.speak_dialog('unset.cruise.control')
 
     def handle_set_leds(self, message):
-        color = "red"
+        color = message.data.get("color", "red")
         self.speak_dialog('leds', data={'red':red})
 
     def handle_leds_off(self, message):
         self.speak_dialog('leds.off')
 
     def handle_windshield_on_wipers(self, message):
-        speed = "3"
+        speed = message.data.get("speed", "120")
         self.speak_dialog('windshield.wipers', data={'speed':speed})
 
     def handle_windshield_off_wipers(self, message):
